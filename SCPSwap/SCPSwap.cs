3c53fc527ad25c3620b1cf339ba7ede952dfe810
Thomasjosif <thomasjosif@outlook.com>
2020-11-05 22:33:47 -0500

    Bump version

--- SCPSwap/SCPSwap.cs
+++ SCPSwap/SCPSwap.cs
@@ -11,8 +11,8 @@ namespace ScpSwap
 	{
 		public EventHandlers Handler { get; private set; }
 		public override string Name => nameof(ScpSwap);
-		public override string Author => "Cyanox";
-		public override Version Version { get; } = new Version(2, 6, 1);
+		public override string Author => "Originally written by Cyanox, modifications by KoukoCocoa & Thomasjosif";
+		public override Version Version { get; } = new Version(1, 3, 0);
 
 		public ScpSwap() { }
 
